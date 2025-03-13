7b045b18957a0f96a96c3c487527e3f0c06278c8
anonboy096 <athicalhacker2005@gmail.com>
2025-03-13 21:29:15 +0530

    Update header.jsx

--- components/page/header.jsx
+++ components/page/header.jsx
@@ -6,6 +6,7 @@ import Search from "./search";
 import { ChevronDown, ChevronLeft, Share2 } from "lucide-react";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import { AiFillInstagram } from "react-icons/ai"; // Import if using react-icons
 
 export default function Header() {
     const path = usePathname();
@@ -19,7 +20,16 @@ export default function Header() {
                     <Button size="icon" asChild variant="outline" className="rounded-full"><Link href="/"><ChevronLeft className="w-5 h-5" /></Link></Button>
                 )}
                 <Logo />  
-                <ModeToggle />
+                <div className="flex gap-4 items-center">
+                    <ModeToggle />
+                    <a href="https://www.instagram.com/_umangggg__" target="_blank" rel="noopener noreferrer">
+                        {/* Using online logo */}
+                        { <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/768px-Instagram_logo_2016.svg.png" alt="Instagram Logo" className="w-6 h-6" /> }
+                        
+                        {/* Using react-icons */}
+                        
+                    </a>
+                </div>
             </div>
             
             <Search />
